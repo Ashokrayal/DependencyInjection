f79503e063e57c685aa3fdfa49d486440f9c4ba6
Ashok Rayal <ashokrayal27@gmail.com>
2018-10-07 12:59:09 +0530

    Add project files.

---
+++ MethodInjection/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Unity;
+using Unity.Attributes;
+
+namespace MethodInjection
+{
+    public interface IEmployee
+    {
+    }
+    public class Employee : IEmployee
+    {
+        private ICompany _Company;
+
+        [InjectionMethod]
+        public void SetObject(ICompany tmpCompany)
+        {
+            _Company = tmpCompany;
+        }
+
+        public void DisplaySalary()
+        {
+            _Company.ShowSalary();
+        }
+    }
+
+    public interface ICompany
+    {
+        void ShowSalary();
+    }
+    public class Company : ICompany
+    {
+        public void ShowSalary()
+        {
+            Console.WriteLine("Your salary is 40 K");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer unitycontainer = new UnityContainer();
+            unitycontainer.RegisterType<ICompany, Company>();
+
+            Employee emp = unitycontainer.Resolve<Employee>();
+            emp.DisplaySalary();
+
+
+            Console.ReadLine();
+        }
+    }
+}
